518e994f5f5442dc84a69a103bfa5c74da66a959
BigMurloc <dominik.pasymowski@gmail.com>
2023-11-14 12:20:42 +0100

    ThreadUnsafeSingleton - desc

--- src/singleton/ThreadUnsafeSingleton.java
+++ src/singleton/ThreadUnsafeSingleton.java
@@ -2,8 +2,10 @@ package singleton;
 
 /**
  * This implementation is thread unsafe. <p></p>
- * TODO explain why with details <p></p>
  * When calling getInstance() by multiple threads the INSTANCE variable might be null to X amount of them thus creating multiple new objects
+ * <p></p>
+ * This is because of the memory visibility,
+ * by default the writes to variables are not immediately flushed so other threads can see them
  */
 class ThreadUnsafeSingleton {
 
